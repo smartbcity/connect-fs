e2d3728420ebe70fb0d7b28bf7dc77258d38d945
Adrien Poupard <adrien@smartb.city>
2023-01-11 00:34:45 +0100

    Clean useless lib

--- fs-api/api-gateway/build.gradle.kts
+++ fs-api/api-gateway/build.gradle.kts
@@ -7,10 +7,6 @@ plugins {
 
 dependencies {
     api("city.smartb.f2:f2-spring-boot-starter-function-http:${Versions.f2}")
-// https://mvnrepository.com/artifact/org.springframework/spring-core
-    implementation("org.springframework:spring-core:6.0.3")
-    implementation("org.springframework:spring-context:6.0.3")
-
 
     implementation("city.smartb.ssm:ssm-tx-config-spring-boot-starter:${Versions.s2}")
     implementation(project(":fs-api:api-config"))

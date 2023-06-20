ca43585d71b2c0fab3401b3e6ab256297dc0230d
Luc Heydel <luc@smartb.city>
2023-06-20 14:40:20 +0200

    extend vectorpedia timeout

--- fs-s2/file/file-app/src/main/kotlin/city/smartb/fs/s2/file/app/service/VectorpediaClient.kt
+++ fs-s2/file/file-app/src/main/kotlin/city/smartb/fs/s2/file/app/service/VectorpediaClient.kt
@@ -2,12 +2,17 @@ package city.smartb.fs.s2.file.app.service
 
 import city.smartb.fs.s2.file.client.Client
 import city.smartb.fs.s2.file.domain.model.FilePath
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.plugins.HttpTimeout
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-class VectorpediaClient(baseUrl: String): Client(baseUrl) {
+class VectorpediaClient(
+    baseUrl: String,
+    block: HttpClientConfig<*>.() -> Unit = {}
+): Client(baseUrl, block) {
     suspend fun fileVectorize(path: FilePath, file: ByteArray, metadata: Map<String, String>): Unit = postFormData("") {
         file("file", file, path.name)
         param("metadata", metadata + ("fsPath" to path.toString()))
@@ -21,5 +26,9 @@ class VectorpediaConfiguration {
     lateinit var vectorpediaUrl: String
 
     @Bean
-    fun vectorpediaClient() = VectorpediaClient(vectorpediaUrl)
+    fun vectorpediaClient() = VectorpediaClient(vectorpediaUrl) {
+        install(HttpTimeout) {
+            requestTimeoutMillis = 60000
+        }
+    }
 }
